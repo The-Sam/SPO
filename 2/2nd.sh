3e631a150aec0b2e4c6763afa4667a9069a6d93b
The-Sam <36762105+The-Sam@users.noreply.github.com>
2020-03-24 21:11:21 +0300

    Add files via upload

---
+++ 2/2nd.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clone https://github.com/Bitmessage/PyBitmessage $HOME/PyBitmessage #загружаем репозиторий
+sudo apt-get install python openssl libssl-dev git python-msgpack python-qt4 #resolve dependencies
+cd $HOME/PyBitmessage
+git pull #upgrade
+~/PyBitmessage/src/bitmessagemain.py #run
